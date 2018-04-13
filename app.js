eae7faba92022604884703f31868c701392cb463
andrewhlu <alu@ucsb.edu>
2018-04-13 14:19:58 -0700

    trying fetch instead

--- app.js
+++ app.js
@@ -157,14 +157,22 @@ bot.dialog('FindDC', [
 			session.say("Oh no. It looks like all the Dining Commons have closed for the remainder of the day. Sorry!");
 		}
 		else {
-			request(requestString, function (error, response, body) {
-				console.log('error:', error); // Print the error if one occurred
-				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-				requestBody = body;
-				console.log("Request Body 1: " + requestBody);
-			});
-			console.log("Request Body 2: " + requestBody);
+			// request(requestString, function (error, response, body) {
+			// 	console.log('error:', error); // Print the error if one occurred
+			// 	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+			// 	requestBody = body;
+			// 	console.log("Request Body 1: " + requestBody);
+			// });
+			// console.log("Request Body 1: " + requestBody);
 
+			fetch(requestString)
+		    .then(res => res.text())
+		    .then(body => {
+		    	requestBody = body;
+		    	console.log("Request Body 1: " + requestBody);
+		    });
+
+			console.log("Request Body 1: " + requestBody);
 			//Extract menus from website
 			const analyze = cheerio.load(requestBody);
 			var carrilloBody = analyze('#Carrillo-body .panel-body').html();
