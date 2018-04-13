4d2a7d99aa2432f11f43ceee2dac03ff7bedf72f
andrewhlu <alu@ucsb.edu>
2018-04-13 14:04:59 -0700

    test

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ bot.dialog('FindDC', [
 				console.log('error:', error); // Print the error if one occurred
 				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 				requestBody = body;
+				console.log(requestBody);
 			});
 
 			//Extract menus from website
